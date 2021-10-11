f3ef07a6f21c5ea7d14cea8ff70cdd2a41d8efd7
hajrudinvejzovic <92256420+hajrudinvejzovic@users.noreply.github.com>
2021-10-11 14:01:26 +0200

    Java homework week 1

---
+++ glavnaklasa.java
@@ -0,0 +1,64 @@
+package haha;
+import java.util.Scanner;
+
+public class glavnaklasa {
+  public static void main(String[]args) {
+	  /*1.
+	   * System.out.println("Hajrudin Vejzoic");
+	  2.
+	  System.out.println("Hello World");
+	  System.out.print("And to all people around");*/
+	  
+	  /*3.
+	   * int x = 60*60*24*365;
+	  System.out.print("This is seconds that one year got: " + " " + x ); */
+	  
+	  /*4.
+	   * Scanner ispis = new Scanner(System.in);
+	  System.out.print("Unesite prvi broj kako biste izraèunali nešto: ");
+	  int prvi_broj= Integer.parseInt(ispis.nextLine());
+	  System.out.print("Unesite drugi broj: ");
+	  int drugi_broj= Integer.parseInt(ispis.nextLine());
+	  int result = prvi_broj+drugi_broj;
+	  System.out.print("Ovo je zbir dva broja: " + result); */
+	  
+	  /*5.
+	   * Scanner ispis = new Scanner(System.in);
+	  System.out.print("Unesite broj za provjeru: ");
+	  int broj= Integer.parseInt(ispis.nextLine());
+	  if(broj>0) {
+	    System.out.print("Ovaj broj je veci od 0.");
+	  }else {
+		  System.out.print("Ovaj broj je manji od 0.");*/
+	  
+      
+      /*6.
+	  Scanner ispis= new Scanner(System.in);
+      System.out.print("Upiši prvi broj: ");
+      int a = Integer.parseInt(ispis.nextLine());
+      System.out.print("Unesi drugi broj: ");
+      int b= Integer.parseInt(ispis.nextLine());
+      if(a>b) {
+        System.out.print("a je veæi od b.");
+      }else if (a==b) {
+    	System.out.println("a je jednak b: ");
+      }else if(a<b){
+      	System.out.println("b je veci od a: ");*/
+	  
+	  
+	  
+      }
+  }
+
+      
+  
+  
+  
+	  
+	  
+	  
+	  
+	  
+	  
+  
+
